ac23d112227f3541731590945c04175bc9564e74
John Erick <johnerick.dev@outlook.com>
2022-06-14 23:26:33 -0300

    Commit inicial

---
+++ src/respostas/desafio2.ts
@@ -0,0 +1,23 @@
+enum Trabalho {
+    Atriz,
+    Padeiro
+}
+
+type Humano = {
+    nome: string,
+    idade: number,
+    profissao: Trabalho
+}
+
+let pessoa1: Humano = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Trabalho.Atriz
+};
+
+let pessoa2: Humano = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Trabalho.Padeiro
+};
+
